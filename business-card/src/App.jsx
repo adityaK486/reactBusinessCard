1408838ee02aa9f0b13e36e6e0bb2b063b175567
Aditya Kumar <100521099+adityaK486@users.noreply.github.com>
2024-01-24 13:23:00 +0000

    Pending changes exported from your codespace

---
+++ business-card/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const info = {
+    name:"Lokeshwar",
+    desc:"A TA in 100xDevs Cohort2.0",
+    interest:["Ionic","OPen Source","App Dev"],
+    linkedIn:"www.linkedIn.com",
+    twitter:"https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+  }
+
+  return (
+    <div>
+       <Card info={info}></Card>
+    </div>
+  )
+}
+
+function Card(props){
+  const info = props.info;
+  return (
+    <div className='card'>
+      <h1>{info.name}</h1>
+      <p>{info.desc}</p>
+      <h1>Interests</h1>Ability to pass in props to the Component
+      <ul>
+         {info.interest.map(function(i){
+          return <li>
+            {i}
+          </li>
+         })}
+      </ul>
+     <div className='links'>
+        <a href={info.linkedIn} ><button>LinkedIn</button></a>
+        <a href={info.twitter}><button>Twitter</button></a>
+     </div>
+    </div>
+  )
+}
+
+export default App
